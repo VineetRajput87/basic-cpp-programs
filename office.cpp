8733efba58d1e4fb69a7ae9d99abd6bfb0b3ed04
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2022-12-24 17:52:41 +0530

    Add files via upload

---
+++ office.cpp
@@ -0,0 +1,21 @@
+// // Q. :- Ram wants to become fit for which he decided to walk to the office and return home by walking. 
+// It is known that Ram's office is X km away from his home.
+// // If his office is open on 5 days in a week, 
+// find the number of kilometers Ram travels through office trips in a week.
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int t,a;
+    cout<<"How Many Times Do you want to continue : ";
+    cin>>t;
+    while(t--)
+    {
+        cout<<"Enter kilometers : ";
+        cin>>a;
+        cout<<"Chef has to travel "<<a*10<<"kms in total."<<endl;
+    }
+	return 0;
+}
