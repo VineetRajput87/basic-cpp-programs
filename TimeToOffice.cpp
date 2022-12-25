51e1b5ef09dbbb11988548a5159176d6ac12600f
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2022-12-25 15:33:28 +0530

    Create TimeToOffice.cpp

---
+++ TimeToOffice.cpp
@@ -0,0 +1,23 @@
+// Q.:- Ravi has recently moved into an apartment. It takes 30 minutes for Ravi to reach office from the apartment.
+//      Ravi left for the office X minutes before Ravi was
+//      supposed to reach. Determine whether or not Ravi will
+//      be able to reach on time.
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int t,a;
+    cout<<"How many times you want to repeat : ";
+    cin>>t;
+    while(t--)
+    {
+        cout<<"Ravi leaves at how much minutes before : ";
+        cin>>a;
+        if(a>=30)
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;
+    }
+	return 0;
+}
