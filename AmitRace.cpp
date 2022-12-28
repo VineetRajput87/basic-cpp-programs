090252c2637d22d607ab925a53280ad8e2f5989b
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2022-12-28 23:12:25 +0530

    Create AmitRace.cpp

---
+++ AmitRace.cpp
@@ -0,0 +1,24 @@
+// Q.:- Amit got his dream seat in F1 and secured a 3rd
+// place in his debut race. He now wants to know the time gap 
+// between him and the winner of the race.
+
+// You are his Amit engineer and you only know the time gap 
+//   between Amit and the runner up of the race, given as A 
+//   seconds,and the time gap between the runner up and the winner of the race, given as B seconds.
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int t;
+    cout<<"How many times do you want to continue : ";
+    cin>>t;
+    while(t--)
+    {
+        int a,b;
+        cout<<"Enter the A and B : ";
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+	return 0;
+}
