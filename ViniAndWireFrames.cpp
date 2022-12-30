54f12c0d7005feb5981cb3809f6a801a0acdd620
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2022-12-30 20:41:43 +0530

    Create ViniAndWireFrames.cpp

---
+++ ViniAndWireFrames.cpp
@@ -0,0 +1,19 @@
+// Q.:- Vineet has a rectangular plate of length N cm and width M cm. He wants to make a wireframe around the plate. The wireframe costs X rupees per cm.
+//     Determine the cost Vineet needs to incur to buy the wireframe.
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int t;
+	cout<<"How many times do you want to continue : ";
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,c;
+	    cout<<"Enter the value of A,B and C : ";
+	    cin>>a>>b>>c;
+	    cout<<((a*2)+(b*2))*c<<endl;
+	}
+	return 0;
+}
