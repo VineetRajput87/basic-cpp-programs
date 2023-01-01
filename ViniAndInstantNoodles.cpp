996f90964b35a37d64e723ce84dc3121de510760
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-01-01 20:06:33 +0530

    Create ViniAndInstantNoodles.cpp

---
+++ ViniAndInstantNoodles.cpp
@@ -0,0 +1,13 @@
+// Q.:- Vineet has invented 1-minute Instant Noodles. As the name suggests, each packet takes exactly 1 minute to cook.Vineet's restaurant has X stoves and only 1 packet can be cooked in a single pan. How many customers can Vineet serve in Y minutes if each customer orders exactly 1 packet of noodles?
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int a,b;
+	cout<<"Enter A and B : ";
+	cin>>a>>b;
+	cout<<a*b<<endl;
+	return 0;
+}
