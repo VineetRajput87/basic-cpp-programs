6614246f2a5cc1dcecbf54a2179207088db90cc7
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-01-03 20:51:02 +0530

    Create VineetAndChocolates.cpp

---
+++ VineetAndChocolates.cpp
@@ -0,0 +1,19 @@
+// Q.:- Vineet wants to gift C chocolates to Botswal on his birthday. However, he has only X chocolates with him. The cost of 1 chocolate is Y rupees. Find the minimum money in rupees Chef needs to spend so that he can gift C chocolates to Botswal.
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int t;
+	cout<<"How many times do you want to continue : ";
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,c;
+	    cout<<"Enter the value of A, B and C : ";
+	    cin>>a>>b>>c;
+	    cout<<(a-b)*c<<endl;
+	}
+	return 0;
+}
