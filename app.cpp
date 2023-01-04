d7c02e75b6e177222d14de972dc82a6fddc4e945
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-01-04 23:59:48 +0530

    Create app.cpp

---
+++ app.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++){
+	    int a,b,flag=0;
+	    cin>>a>>b;
+	    if(a==1){
+	        cout<<1<<endl;
+	        continue;
+	    }
+	    while(a!=b){
+	        if(a>b)
+	        a-=b;
+	        else 
+	        b-=a;
+	    }
+	    cout<<a<<endl;
+	}
+	return 0;
+}
