510205145a7bd6554fe6b81d8d3be94fb286065e
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-01-07 20:32:05 +0530

    Create SpiceLevel.cpp

---
+++ SpiceLevel.cpp
@@ -0,0 +1,24 @@
+// Q.:- Each item in Vineet’s menu is assigned a spice level from 1 to 10. Based on the spice level, the item is categorised as:
+
+//     MILD: If the spice level is less than 4.
+//     MEDIUM: If the spice level is greater than equal to 4 but
+//     less than 7.
+//     HOT: If the spice level is greater than equal to 7.
+//     Given that the spice level of an item is X, find the
+//     category it lies in.
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+	    int a;
+	    cout<<"Enter the value of A : ";
+	    cin>>a;
+	    if(a<4)
+	    cout<<"MILD"<<endl;
+	    else if(a>=4 && a<7)
+	    cout<<"MEDIUM"<<endl;
+	    else
+	    cout<<"HOT"<<endl;
+	return 0;
+}
