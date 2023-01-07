446deb7a1a8645dfa87d4b52721d8f2f27f69347
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-01-07 20:36:35 +0530

    Create TrueAndFalsePaper.cpp

---
+++ TrueAndFalsePaper.cpp
@@ -0,0 +1,17 @@
+// Q.:- Alice wrote an exam containing NN true or false questions (i.e. questions whose answer is either true or false). Each question is worth 1 mark and there is no negative marking in the examination. Alice scored K marks out of N.
+//     Bob wrote the same exam but he marked each and every
+//     question as the opposite of what Alice did, i.e, for
+//     whichever questions Alice marked true, Bob marked false and
+//     for whichever questions Alice marked false, Bob marked true.
+//     Determine the score of Bob.
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+	    int a,b;
+	    cout<<"Enter the value of A and B : ";
+	    cin>>a>>b;
+	    cout<<a-b<<endl;
+	return 0;
+}
