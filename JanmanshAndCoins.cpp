75cabdbadc8f424e53c112f0a829fc035f844700
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-01-10 19:59:41 +0530

    Create JanmanshAndCoins.cpp

---
+++ JanmanshAndCoins.cpp
@@ -0,0 +1,13 @@
+// Q.:- Janmansh received X coins of 10 rupees and Y coins of 5 rupees from Chingari. Since he is weak in math, can you find out how much total money does he have?
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	    int a,b;
+	    cout<<"Enter the value of X and Y : ";
+	    cin>>a>>b;
+	    cout<<(10*a)+(b*5)<<endl;
+	return 0;
+}
