16941ff6c65e7a9eebadb8b68de621ceb9c913ee
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-01-15 20:47:24 +0530

    Create BobAtTheBank.cpp

---
+++ BobAtTheBank.cpp
@@ -0,0 +1,23 @@
+// Q.:- Bob has an account in the Bobby Bank. His current account balance is W rupees.
+
+// Each month, the office in which Bob works deposits a fixed amount of X rupees to his account.
+// Y rupees is deducted from Bob's account each month as bank charges.
+
+// Find his final account balance after Z months. Note that the account balance can be negative as well.
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,c,d;
+	    cin>>a>>b>>c>>d;
+	    cout<<(a+((b-c)*d))<<endl;
+	}
+	return 0;
+}
+
