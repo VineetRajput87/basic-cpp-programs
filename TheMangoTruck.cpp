df9584d8849b3a83b708da2b60f8de981dd1d7a5
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-01-15 20:59:36 +0530

    Create TheMangoTruck.cpp

---
+++ TheMangoTruck.cpp
@@ -0,0 +1,24 @@
+// Q.:- You are given that a mango weighs X kilograms and a truck weighs Y kilograms. You want to cross a bridge that can withstand a weight of Z kilograms.
+//     Find the maximum number of mangoes you can load in the truck
+//     so that you can cross the bridge safely.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,c,i=1;
+	    cin>>a>>b>>c;
+	    while(((i*a)+b)<=c)
+	    {
+	        i++;
+	    }
+	    i-=1;
+	    cout<<i<<endl;
+	}
+	return 0;
+}
