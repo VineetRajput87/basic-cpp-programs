5769bf2fea53beed56bc4fce628688584fe43f04
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-01-15 20:31:51 +0530

    Create TyreProblem.cpp

---
+++ TyreProblem.cpp
@@ -0,0 +1,22 @@
+// Q.:- There are N bikes and M cars on the road.
+
+// 1. Each bike has 2 tyres.
+// 2. Each car has 4 tyres.
+
+// Find the total number of tyres on the road.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    cout<<a*2+b*4<<endl;
+	}
+	return 0;
+}
