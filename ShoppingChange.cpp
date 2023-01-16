e6356fd1e81c97288e9d687ba1ef56e4a075aff5
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-01-16 21:46:19 +0530

    Create ShoppingChange.cpp

---
+++ ShoppingChange.cpp
@@ -0,0 +1,20 @@
+// Q.:- Vineet went shopping and bought items worth X rupees
+//      1 ≤ X ≤ 100). Unfortunately, Vineet only has a single 100 rupees note.
+
+// Since Vineet is weak at maths, can you help Vineet in calculating what money he should get back after paying 100 rupees for those items?
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a;
+	    cin>>a;
+	    cout<<100-a<<endl;
+	}
+	return 0;
+}
