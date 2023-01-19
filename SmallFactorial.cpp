56c8c448afecd0cc9a88d82079f47be70898fab1
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-01-19 19:27:24 +0530

    Create SmallFactorial.cpp

---
+++ SmallFactorial.cpp
@@ -0,0 +1,21 @@
+// Q.:- Write a program to find the factorial value of any number entered by the user.
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,i,temp=1;
+	    cin>>a;
+	    for(i=a;i>0;i--)
+	    {
+	        temp=temp*i;
+	    }
+	    cout<<temp<<endl;
+	}
+	return 0;
+}
