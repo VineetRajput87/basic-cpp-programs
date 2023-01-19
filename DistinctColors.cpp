10adc9e27a9eadcedde5d372e9b30be5fb83f497
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-01-19 19:43:09 +0530

    Create DistinctColors.cpp

---
+++ DistinctColors.cpp
@@ -0,0 +1,26 @@
+// Q.:- There are N different types of colours numbered from 1 to N. Vineet has A_i balls having colour i,( 1 ≤ i ≤ N ).
+
+// Vineet will arrange some boxes and put each ball in exactly one of those boxes.
+// Find the minimum number of boxes Vineet needs so that no box contains two balls of same colour.
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,x=0;
+	    cin>>a;
+	    int brr[a];
+	    for(int i=0;i<a; i++)
+	    {
+	        cin>>brr[i];
+	        if(brr[i]>x)
+	        x=brr[i];
+	    }
+	    cout<<x<<endl;
+	}
+	return 0;
+}
