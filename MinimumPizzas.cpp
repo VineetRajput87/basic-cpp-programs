7fa478e0099bdcb4a4a1e4910c8f4a3640612614
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-01-22 20:42:56 +0530

    Create MinimumPizzas.cpp

---
+++ MinimumPizzas.cpp
@@ -0,0 +1,21 @@
+// Q.:- Each pizza consists of 4 slices. There are N friends and each friend needs exactly X slices.
+
+// Find the minimum number of pizzas they should order to satisfy their appetite.
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    if((a*b)%4==0)
+	    cout<<(a*b)/4<<endl;
+	    else
+	    cout<<((a*b)/4)+1<<endl;
+	}
+	return 0;
+}
