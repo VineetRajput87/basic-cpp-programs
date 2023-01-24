c896c75a8fbc13e9dc1beb56c5f2b82483eaae22
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-01-24 21:15:41 +0530

    Create GoodProgram.cpp

---
+++ GoodProgram.cpp
@@ -0,0 +1,26 @@
+// Q.:- In computing, the collection of four bits is called a nibble.
+
+// Vineet defines a program as:
+
+// 1. Good, if it takes exactly X nibbles of memory, where X is a positive integer.
+// 2. Not Good, otherwise.
+// Given a program which takes  bits of memory, determine whether it is Good or Not Good.
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a;
+	    cin>>a;
+	    if(a%4==0)
+	    cout<<"Good"<<endl;
+	    else
+	    cout<<"Not Good"<<endl;
+	}
+	return 0;
+}
