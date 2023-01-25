62630f6a089bf5fd13430ed6c4d6e4a5793f0b3d
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-01-25 18:16:36 +0530

    Create WatchingMovieAt2x.cpp

---
+++ WatchingMovieAt2x.cpp
@@ -0,0 +1,18 @@
+// Q.:- Vineet started watching a movie that runs for a total of X minutes.
+
+// Vineet has decided to watch the first Y minutes of the movie at twice the usual speed as he was warned by his friends that the movie gets interesting only after the first Y minutes.
+
+// How long will Vineet spend watching the movie in total?
+
+// Note: It is guaranteed that Y is even.
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	    int a,b;
+	    cin>>a>>b;
+	    cout<<a-(b/2)<<endl;
+	return 0;
+}
