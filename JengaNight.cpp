aadbcecd9baff1a216ca5b3d20bd209f4ff60360
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-01-25 17:58:37 +0530

    Create JengaNight.cpp

---
+++ JengaNight.cpp
@@ -0,0 +1,27 @@
+// Q.:- Vineet hosts a party for his birthday. There are N people at the party. All these N people decide to play Jenga.
+
+// There are X Jenga tiles available. In one round, all the players pick 1 tile each and place it in the tower.
+// The game is valid if:
+
+// 1. All the players have a tile in each round;
+// 2. All the tiles are used at the end.
+// Given N and X, find whether the game is valid.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    if(b%a==0)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
