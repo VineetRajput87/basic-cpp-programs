8d65efc2a0a853203edd46be8c1f364c63c7d1d8
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-01-28 22:49:46 +0530

    Create SugarcaneJuiceBusiness.cpp

---
+++ SugarcaneJuiceBusiness.cpp
@@ -0,0 +1,23 @@
+// While Alice was drinking sugarcane juice, she started wondering about the following facts:
+
+// The juicer sells each glass of sugarcane juice for 50 coins.
+// He spends 20% of his total income on buying sugarcane.
+// He spends 20% of his total income on buying salt and mint leaves.
+// He spends 30% of his total income on shop rent.
+// Alice wonders, what is the juicer's profit (in coins) when he sells N glasses of sugarcane juice?
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a;
+	    cin>>a;
+	    cout<<(50*a)-((((50*a)*20)/100) + (((50*a)*20)/100)+ (((50*a)*30)/100))<<endl;
+	}
+	return 0;
+}
