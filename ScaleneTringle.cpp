bf9114263501d7e93ede2e9c940a55a57ad7bda9
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-02-12 20:40:53 +0530

    Update ScaleneTringle.cpp

--- ScaleneTringle.cpp
+++ ScaleneTringle.cpp
@@ -1,6 +1,6 @@
 // Q.:- Given A, B and C as the sides of a triangle, find whether the triangle is scalene.
-// Note:   A triangle is said to be scalene if all three sides of the triangle are distinct.
-//     It is guaranteed that the sides represent a valid triangle.
+// Note: A triangle is said to be scalene if all three sides of the triangle are distinct.
+// It is guaranteed that the sides represent a valid triangle.
 #include <iostream>
 using namespace std;
 int main() 
