acaf54e01b6995a5d797b20d3fbe7de0488e47c6
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-02-16 20:37:40 +0530

    Update NegativeProduct.cpp

--- NegativeProduct.cpp
+++ NegativeProduct.cpp
@@ -1,4 +1,5 @@
 // Q.:- Vineet is given three numbers A,B and C.
+
 //      He wants to find whether he can select exactly two numbers
 //      out of these such that the product of the selected numbers
 //      is negative.
