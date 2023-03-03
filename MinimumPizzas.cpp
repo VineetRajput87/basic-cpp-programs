4287a4e674d3eb80954c0389d00ac68533aab5a6
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-03-03 22:19:35 +0530

    Update MinimumPizzas.cpp

--- MinimumPizzas.cpp
+++ MinimumPizzas.cpp
@@ -1,5 +1,4 @@
 // Q.:- Each pizza consists of 4 slices. There are N friends and each friend needs exactly X slices.
-
 // Find the minimum number of pizzas they should order to satisfy their appetite.
 #include <iostream>
 using namespace std;
