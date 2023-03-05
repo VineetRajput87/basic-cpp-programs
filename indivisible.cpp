66228f760d1baa2363db0db51fc71b62effd6813
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-03-05 11:15:04 +0530

    Update indivisible.cpp

--- indivisible.cpp
+++ indivisible.cpp
@@ -11,9 +11,12 @@ int main()
 	    cin>>a>>b>>c;
 	    for(i=2;i<=100;i++)
 	    {
-	        if((a%i)!=0){
-	          if((b%i)!=0){
-	              if((c%i)!=0){
+	        if((a%i)!=0)
+		{
+	          if((b%i)!=0)
+		  {
+	              if((c%i)!=0)
+		      {
 	                cout<<i<<endl;
 	                break;
 	              }
