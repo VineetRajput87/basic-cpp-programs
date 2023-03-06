1acdac1947c63253ed4a0e0987ed4cee24ddd81e
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-03-06 16:07:00 +0530

    Update IncreaseIQ.cpp

--- IncreaseIQ.cpp
+++ IncreaseIQ.cpp
@@ -1,6 +1,6 @@
 // Q.:- A study has shown that playing a musical instrument helps in increasing one's IQ by 7 points. Vineet knows he can't beat Einstein in physics, but he wants to try to beat him in an IQ competition.
 //     You know that Einstein had an IQ of 170, and Vineet
-//     currently has an IQ of X.
+//     currently has an IQ of X. 
 //     Determine if, after learning to play a musical instrument,
 //     Vineet's IQ will become strictly greater than Einstein's.
 //     Print "Yes" if it is possible for Chef to beat Einstein,
