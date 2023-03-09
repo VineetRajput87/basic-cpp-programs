af6fffe5c78bbea407328070a31fe65d0282765e
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-03-09 22:24:00 +0530

    Update IntegerThatSumToTheSameValue.cpp

--- IntegerThatSumToTheSameValue.cpp
+++ IntegerThatSumToTheSameValue.cpp
@@ -1,7 +1,7 @@
 // Q.:- Vineet has a positive integer N. he is wondering how many ordered pairs of positive integers (i,j) exist such that i+j=N.
 
 // Help Vineet figure out the answer.
-
+  
 // Note that since the pairs are ordered, (1,2) and (2,1) are considered different.
 
 #include <iostream>
