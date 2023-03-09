c6b8ed29e1102418bcf9665c511426be950c2968
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-03-09 22:22:28 +0530

    Update CountTheNotebooks.cpp

--- CountTheNotebooks.cpp
+++ CountTheNotebooks.cpp
@@ -1,7 +1,7 @@
 // Q.:- You know that 1 kg of pulp can be used to make 1000 pages and 1 notebook consists of 100 pages.
 
 //   Suppose a notebook factory receives N kg of pulp, how many
-//   notebooks can be made from that?
+//   notebooks can be made from that? 
 
 #include <iostream>
 using namespace std;
