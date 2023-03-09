84235e07a79b178d8e859b643d9999a073a7cac1
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-03-09 22:23:08 +0530

    Update VineetDrinksTea.cpp

--- VineetDrinksTea.cpp
+++ VineetDrinksTea.cpp
@@ -3,7 +3,7 @@
 // Vineet can visit the tea shop to refill the jar. In each refill, the jar is completely filled to the brim and Vineet is charged Z rupees.
 // Vineet wonders what is the minimum amount of money he has to pay for drinking exactly X liters of tea daily.
 
-#include <iostream>
+#include <iostream> 
 using namespace std;
 
 int main() 
