3446e6b2aaea34df485e7e032bc6968dc16bd7bb
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-03-13 20:14:25 +0530

    Update HoopJump.cpp

--- HoopJump.cpp
+++ HoopJump.cpp
@@ -1,5 +1,4 @@
 // Q.:- You and your friend are playing a game with hoops. There are N hoops (where N is odd) in a row. You jump into hoop 1, and your friend jumps into hoop N. Then you jump into hoop 2, and after that, your friend jumps into hoop N-1, and so on. The process ends when someone cannot make the next jump because the hoop is occupied by the other person. Find the last hoop that will be jumped into.
-
 #include <iostream>
 using namespace std;
 int main() 
