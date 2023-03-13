6ca6fc2a7916c5a9a516fd41899d35e5e67f8d0b
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-03-13 20:14:04 +0530

    Update EzioAndGuards.cpp

--- EzioAndGuards.cpp
+++ EzioAndGuards.cpp
@@ -1,5 +1,4 @@
 // Q.:-   Ezio can manipulate at most X number of guards with the apple of eden. Given that there are Y number of guards, predict if he can safely manipulate all of them.
-
 #include <iostream>
 using namespace std;
 int main() 
