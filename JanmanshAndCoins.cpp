68047a759abd2d0fc2ffbe82b573e38183bcee68
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-03-14 20:03:12 +0530

    Update JanmanshAndCoins.cpp

--- JanmanshAndCoins.cpp
+++ JanmanshAndCoins.cpp
@@ -1,5 +1,4 @@
 // Q.:- Janmansh received X coins of 10 rupees and Y coins of 5 rupees from Chingari. Since he is weak in math, can you find out how much total money does he have?
-
 #include <iostream>
 using namespace std;
 
