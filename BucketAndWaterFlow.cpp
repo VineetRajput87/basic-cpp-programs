d864ce780ba7a1f28da031c8f85372800b5937f4
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-03-14 20:02:52 +0530

    Update BucketAndWaterFlow.cpp

--- BucketAndWaterFlow.cpp
+++ BucketAndWaterFlow.cpp
@@ -1,6 +1,6 @@
 // Q.:- Vineet has a bucket of water initially having W litres of water in it. The maximum capacity of the bucket is X liters. Vineet turned on the tap and the water starts flowing into the bucket at a rate of Y litres/hour. She left the tap running for exactly Z hours. 
-//     Determine whether the bucket has been overflown,filled exactly,
-//     or is still left unfilled.
+//    Determine whether the bucket has been overflown,filled exactly,
+//    or is still left unfilled.
 
 #include <iostream>
 using namespace std;
