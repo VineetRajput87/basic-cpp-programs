98e49eaf0be50ab7d9035a2a3df16d872cf26bfb
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-03-16 00:01:18 +0530

    Update ExpertSetter.cpp

--- ExpertSetter.cpp
+++ ExpertSetter.cpp
@@ -1,5 +1,4 @@
 // Q.:- A problem setter is called an expert if at least 50% of their problems are approved by Vineet.
-
 //     Munchy submitted X problems for approval. If Y problems out
 //     of those were approved, 
 //     find whether Munchy is an expert or not.
