16aac3d5f0bf6982f128cda260595a27d2292c32
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-03-24 21:38:12 +0530

    Update Fever.cpp

--- Fever.cpp
+++ Fever.cpp
@@ -1,6 +1,5 @@
 // Q.:- Mohit is not feeling well today. He measured his body temperature using a thermometer and it came out to be X °F. A person is said to have fever if his body temperature is strictly greater than 98 °F.
 //   Determine if Mohit has fever or not.
-
 #include <iostream>
 using namespace std;
 
