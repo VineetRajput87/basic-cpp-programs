d838a9fca87bfe2b8dd403d00a9a0e7869f7e9cc
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-03-30 22:42:16 +0530

    Update BrokenPhone.cpp

--- BrokenPhone.cpp
+++ BrokenPhone.cpp
@@ -1,5 +1,4 @@
 // Q.:- Vineet broke his phone. He can get it repaired by spending X rupees or he can buy a new phone by spending Y rupees. Vineet wants to spend as little money as possible. Find out if it is better to get the phone repaired or to buy a new phone.
-
 #include <iostream>
 using namespace std;
 
