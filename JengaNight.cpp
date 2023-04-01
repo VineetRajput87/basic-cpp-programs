a33a954c5c7ff5a5cd27fcb8063d640f86ee2aab
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-04-01 19:29:39 +0530

    Update JengaNight.cpp

--- JengaNight.cpp
+++ JengaNight.cpp
@@ -2,7 +2,6 @@
 
 // There are X Jenga tiles available. In one round, all the players pick 1 tile each and place it in the tower.
 // The game is valid if:
-
 // 1. All the players have a tile in each round;
 // 2. All the tiles are used at the end.
 // Given N and X, find whether the game is valid.
