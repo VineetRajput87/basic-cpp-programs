36d46c019780224fd00a44f8e14e4947eef77bd9
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-04-09 19:45:44 +0530

    Update MinimumCoins.cpp

--- MinimumCoins.cpp
+++ MinimumCoins.cpp
@@ -1,5 +1,5 @@
 // Q.:- There are only 2 type of denominations in Vineetland:
-//     Coins worth 1 rupee each
+// Coins worth 1 rupee each
 //     Notes worth 10 rupees each
 //     Vineet wants to pay his friend exactly X rupees. What is
 //     the minimum number of coins Vineet needs to pay exactly X
