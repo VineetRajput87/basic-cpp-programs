7faea8850c5cb09648d222f451a7ef288c58e35d
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-04-16 18:13:51 +0530

    Update HowMuchScholarship.cpp

--- HowMuchScholarship.cpp
+++ HowMuchScholarship.cpp
@@ -1,6 +1,5 @@
 // Q.:- The ZCO Scholarship Contest has just finished, and you finish with a rank of RR. You know that Rank 1 to Rank 50 will get 100% scholarship on the ZCO exam fee and Rank 51 to Rank 100 will get 50% percentage scholarship on the ZCO exam fee. The rest do not get any scholarship.
 //     What percentage of scholarship will you get ?
-
 #include <iostream>
 using namespace std;
 
