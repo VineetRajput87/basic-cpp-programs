ad1903a781648cec828fc2b88d469d935da015e3
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-04-17 23:15:41 +0530

    Update BobAtTheBank.cpp

--- BobAtTheBank.cpp
+++ BobAtTheBank.cpp
@@ -1,5 +1,4 @@
 // Q.:- Bob has an account in the Bobby Bank. His current account balance is W rupees.
-
 // Each month, the office in which Bob works deposits a fixed amount of X rupees to his account.
 // Y rupees is deducted from Bob's account each month as bank charges.
 // Find his final account balance after Z months. Note that the account balance can be negative as well.
