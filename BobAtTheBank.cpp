f5666d57ea834d8b798832df3027857bba939405
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-04-19 20:19:16 +0530

    Update BobAtTheBank.cpp

--- BobAtTheBank.cpp
+++ BobAtTheBank.cpp
@@ -2,7 +2,6 @@
 // Each month, the office in which Bob works deposits a fixed amount of X rupees to his account.
 // Y rupees is deducted from Bob's account each month as bank charges.
 // Find his final account balance after Z months. Note that the account balance can be negative as well.
-
 #include <iostream>
 using namespace std;
 
