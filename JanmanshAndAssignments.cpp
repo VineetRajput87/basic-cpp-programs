0808d460074abb98538d8e0242c96d33406165b9
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-04-22 21:52:07 +0530

    Update JanmanshAndAssignments.cpp

--- JanmanshAndAssignments.cpp
+++ JanmanshAndAssignments.cpp
@@ -1,5 +1,4 @@
 // Q.:- Janmansh has to submit 3 assignments for Chingari before 10 pm and he starts to do the assignments at X pm. Each assignment takes him 1 hour to complete. Can you tell whether he'll be able to complete all assignments on time or not?
-
 #include <iostream>
 using namespace std;
 
