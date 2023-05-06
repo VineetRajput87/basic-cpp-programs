3e10c0fabb6259e4f6bd0ea384f3eb8f2b3ef969
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-05-06 22:57:05 +0530

    Update BathInWinter.cpp

--- BathInWinter.cpp
+++ BathInWinter.cpp
@@ -1,6 +1,6 @@
 // Q.:- A geyser has a capacity of X litres of water and a bucket has a capacity of Y litres of water. 
 // One person requires exactly 2 buckets of water to take a bath. Find the maximum number of people that can take bath using water from one completely filled geyser. 
-#include <iostream> 
+#include <iostream>  
 using namespace std; 
 int main() 
 {
