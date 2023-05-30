1803a982da42a855bada506137452ac75370fea0
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-05-30 21:33:31 +0530

    Update Hackerman.cpp

--- Hackerman.cpp
+++ Hackerman.cpp
@@ -1,7 +1,6 @@
 // Q.:- Hackerman wants to know who is the better player between Bob and Alice with the help of a game.
 
 // The game proceeds as follows:
-
 // First, Alice throws a die and gets the number A
 // Then, Bob throws a die and gets the number B
 // Alice wins the game if the sum on the dice is a prime number; and Bob wins otherwise.
