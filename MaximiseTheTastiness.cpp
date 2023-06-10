4a6312c8dd500d71576f881b205600d0c4512ddc
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-06-10 22:03:39 +0530

    Update MaximiseTheTastiness.cpp

--- MaximiseTheTastiness.cpp
+++ MaximiseTheTastiness.cpp
@@ -1,5 +1,4 @@
 // Q.:- Vineet is making a dish that consists of exactly two ingredients. He has four ingredients A,B,C and D with tastiness a,b,c, and d respectively. He can use either A or B as the first ingredient and either C or D as the second ingredient.
-
 // The tastiness of a dish is the sum of tastiness of its ingredients. Find the maximum possible tastiness of the dish that the chef can prepare.
 
 #include <iostream>
