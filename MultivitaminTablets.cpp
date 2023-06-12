cf115e6c0324585b3c4b901af33c37cc67bc374b
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-06-12 21:43:28 +0530

    Update MultivitaminTablets.cpp

--- MultivitaminTablets.cpp
+++ MultivitaminTablets.cpp
@@ -1,7 +1,7 @@
 // Q.:- The doctor prescribed Vineet to take a multivitamin tablet 3 times a day for the next X days.
 //     Vineet already has Y multivitamin tablets.
 //     Determine if Vineet has enough tablets for these X days
-//     or not.
+//     or not. 
  
 #include <iostream> 
 using namespace std;
