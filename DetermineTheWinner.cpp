a6476d2404f6424f037a29c5011870bdbfdb6571
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-06-17 23:44:45 +0530

    Update DetermineTheWinner.cpp

--- DetermineTheWinner.cpp
+++ DetermineTheWinner.cpp
@@ -3,7 +3,6 @@
 
 // P made AC submissions on problems A and B at time instants P_A and P_B
 //   respectively while Q made AC submissions on problems A and B at time instants Q_A and Q_B.
-
 // It is given that the time penalty is the minimum time instant at which a participant has solved both the problems. Also the participant with the lower time penalty will have a better rank.
 // Determine which participant got the better rank or if there is a TIE.
 
