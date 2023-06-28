fdf3e30f5b19ace5aa99381bb447df64dc3be38c
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-06-28 21:29:53 +0530

    Update ChangeRowAndColumnBoth.cpp

--- ChangeRowAndColumnBoth.cpp
+++ ChangeRowAndColumnBoth.cpp
@@ -1,5 +1,4 @@
 // Q.:- There is a 10×10 grid with rows numbered 1 to 10 from top to bottom, and columns 1 to 10 from left to right. Each cell is identified by a pair (r,c) which means that the cell is located at row r and column c.
-
 // If Vineet's current location is (a,b), then in one move Vineet can go to (c,d) if both of the following are satisfied:
 // 1. a!=c
 // 2. b!=d
