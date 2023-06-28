76b3931dadfc4be11e231c960eebac60f8ab9d4e
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-06-28 21:29:43 +0530

    Update ChangeRowAndColumnBoth.cpp

--- ChangeRowAndColumnBoth.cpp
+++ ChangeRowAndColumnBoth.cpp
@@ -1,7 +1,6 @@
 // Q.:- There is a 10×10 grid with rows numbered 1 to 10 from top to bottom, and columns 1 to 10 from left to right. Each cell is identified by a pair (r,c) which means that the cell is located at row r and column c.
 
 // If Vineet's current location is (a,b), then in one move Vineet can go to (c,d) if both of the following are satisfied:
-
 // 1. a!=c
 // 2. b!=d
 // Determine the minimum number of moves required to go from (s_x,s_y) to (e_x,e_y) .
