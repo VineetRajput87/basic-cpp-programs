a024496983743352c5d3901557d1be3249db4811
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-06-29 19:58:08 +0530

    Update teamA&teamB.cpp

--- teamA&teamB.cpp
+++ teamA&teamB.cpp
@@ -1,6 +1,6 @@
 // Q.:- There is a cricket match going on between two teams A
 //   and B.Team B is batting second and got a target of X 
-//   runs.Currently, team B has scored Y runs. Determine how
+//   runs.Currently, team B has scored Y runs. Determine how 
 //   many more runs Team B should score to win the match.
 // Note: The target score in cricket matches is one more than
 //     the number of runs scored by the team that batted 
