ade27499fc4641267a86ea5a1f47f47c06c6baac
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-07-03 22:21:22 +0530

    Update MonthlyBudget.cpp

--- MonthlyBudget.cpp
+++ MonthlyBudget.cpp
@@ -1,5 +1,4 @@
 // Q.:- Akshat has X rupees to spend in the current month. His daily expenditure is Y rupees, i.e., he spends Y rupees each day.Given that the current month has 30 days, find out if Akshat has enough money to meet his daily expenditures for this month.
-
 #include <iostream>
 using namespace std;
 int main() 
