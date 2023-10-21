c40bbec8e3e1215f0ba7201fe4656e3dc15ee3da
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-10-21 09:57:26 +0530

    Update kingsCars.cpp

--- kingsCars.cpp
+++ kingsCars.cpp
@@ -1,7 +1,7 @@
 // Q.:- King loves to go on tours with his friends.King has N cars that can seat 5 people each and M cars that can seat 7 people each. Determine the maximum number of people that can travel together in these cars.
     
 #include <iostream>
-using namespace std;
+using namespace std; 
 int main() 
 {
     int t,N,M;
