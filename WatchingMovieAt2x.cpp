5134679b7a92543c43c26167eb3d1b63c6215f85
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-10-25 17:53:12 +0530

    Update WatchingMovieAt2x.cpp

--- WatchingMovieAt2x.cpp
+++ WatchingMovieAt2x.cpp
@@ -1,5 +1,6 @@
 // Q.:- Vineet started watching a movie that runs for a total of X minutes.
-// Vineet has decided to watch the first Y minutes of the movie at twice the usual speed as he was warned by his friends that the movie gets interesting only after the first Y minutes.
+// Vineet has decided to watch the first Y minutes of the movie at twice the usual speed as 
+// he was warned by his friends that the movie gets interesting only after the first Y minutes.
 
 // How long will Vineet spend watching the movie in total?
 
