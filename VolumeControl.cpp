db0069faa027dcbe59b3cba2c7bcd82d97b8afd5
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-10-26 22:22:03 +0530

    Update VolumeControl.cpp

--- VolumeControl.cpp
+++ VolumeControl.cpp
@@ -1,5 +1,5 @@
 // Q.:- Vineet is watching TV. The current volume of the TV is X. Pressing the volume up button of the TV remote increases the volume by 1 while pressing the volume down button decreases the volume by 1. Vineet wants to change the volume from X to Y. Find the minimum number of button presses required to do so.
-
+ 
 #include <iostream>
 using namespace std;
   
