412f22ef745ee8eaf17d5a0a0f170be1019e0437
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-10-29 18:48:31 +0530

    Update VineetPlaysLudo.cpp

--- VineetPlaysLudo.cpp
+++ VineetPlaysLudo.cpp
@@ -1,6 +1,5 @@
 // Q.:-Vineet is playing Ludo. According to the rules of Ludo, a player can enter a new token into the play only when he rolls a 6 on the die.
 // In the current turn, Vineet rolled the number X on the die. Determine if Vineet can enter a new token into the play in the current turn or not.
-
 #include <iostream>
 using namespace std;
 
