b2fa1f96e42738db21f75bc0cb67089fe33510b6
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-10-30 22:00:54 +0530

    Update VineetInHisOffice.cpp

--- VineetInHisOffice.cpp
+++ VineetInHisOffice.cpp
@@ -1,6 +1,6 @@
 // Q.:-Recentl Vineet joined a new company. In this company, the employees have to work for X hours each day from Monday to Thursday. Also, in this company, Friday is called Chill Day — employees only have to work for Y hours ( Y < X ) on Friday. Saturdays and Sundays are holidays.
 //     Determine the total number of working hours in one week.
- 
+  
 #include <iostream>
 using namespace std;
 
