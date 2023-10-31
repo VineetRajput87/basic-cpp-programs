5508c5e8ead9d04192039d915e140621dafea237
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-10-31 19:45:04 +0530

    Update VineetDrinksTea.cpp

--- VineetDrinksTea.cpp
+++ VineetDrinksTea.cpp
@@ -1,5 +1,4 @@
 // Q.:- Vineet has planned that he will drink exactly X liters of tea daily. He has an empty jar having a capacity of Y liters.
-
 // Vineet can visit the tea shop to refill the jar. In each refill, the jar is completely filled to the brim and Vineet is charged Z rupees.
 // Vineet wonders what is the minimum amount of money he has to pay for drinking exactly X liters of tea daily.
 
