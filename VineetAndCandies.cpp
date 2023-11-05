419ef7d87f2616e670b9602dfcb8d0dc78d75903
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-11-05 18:29:29 +0530

    Update VineetAndCandies.cpp

--- VineetAndCandies.cpp
+++ VineetAndCandies.cpp
@@ -1,5 +1,4 @@
 // Q.:- There are N children and Vineet wants to give them 1 candy each. Vineet already has X candies with him. To buy the rest, he visits a candy shop. In the shop, packets containing exactly 4 candies are available.
-
 // Determine the minimum number of candy packets Vineet must buy so that he is able to give 1 candy to each of the N children.
 #include <iostream>
 using namespace std; 
