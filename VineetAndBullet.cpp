6fa7855587ca5d2986fd8baf8ace9f31f58ed78b
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-11-06 19:38:45 +0530

    Update VineetAndBullet.cpp

--- VineetAndBullet.cpp
+++ VineetAndBullet.cpp
@@ -1,7 +1,6 @@
 // Q.:- Vineet's bullet moves at X pixels per frame. He wishes to shoot a goomba standing Y pixels away from him. The goomba does not move.
 
 // Find the minimum time (in seconds) after which Vineet should shoot the bullet, such that it hits the goomba after at least Z seconds from now.
-
 #include <iostream> 
 using namespace std;
 
