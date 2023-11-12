59e2383b23bf7eb3769916b383e028c5a2726cea
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-11-12 18:01:02 +0530

    Update TimeToOffice.cpp

--- TimeToOffice.cpp
+++ TimeToOffice.cpp
@@ -1,6 +1,6 @@
 // Q.:- Ravi has recently moved into an apartment. It takes 30 minutes for Ravi to reach office from the apartment.
 //      Ravi left for the office X minutes before Ravi was
-//      supposed to reach. Determine whether or not Ravi will
+//      supposed to reach. Determine whether or not Ravi will 
 //      be able to reach on time. 
 
 #include <iostream>
