ac73f8820fb322b59af253fa34fc5ae0aef66b5c
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-11-17 18:50:19 +0530

    Update TCSexamination.cpp

--- TCSexamination.cpp
+++ TCSexamination.cpp
@@ -1,7 +1,6 @@
 // Q.:- Two friends, Dragon and Sloth, are writing a computer science examination series. There are three subjects in this series: DSA,TOC, and DM. Each subject carries 100 marks.
 
 // You know the individual scores of both Dragon and Sloth in all 3 subjects. You have to determine who got a better rank.
-
 // The rank is decided as follows:
 // The person with a bigger total score gets a better rank.
 // If the total scores are tied, the person who scored higher in
