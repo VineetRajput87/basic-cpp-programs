7165a656965ac430fda3e4cac0233f028cd050df
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-11-21 20:03:18 +0530

    Update SonuFair.cpp

--- SonuFair.cpp
+++ SonuFair.cpp
@@ -1,7 +1,7 @@
 // Q.:- There is a fair going on in sonuland. Sonu wants to visit the fair along with his N friends. Sonu manages to collect K passes for the fair. Will Sonu be able to enter the fair with all his N friends?
 //     A person can enter the fair using one pass, and each pass
 //     can be used by only one person.
- 
+  
 #include <iostream>
 using namespace std; 
 int main() 
