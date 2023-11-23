e23c1e1e5cedb2ac26cb4dcee52c57a90a74b093
Vineet Rajput <114386532+VineetRajput87@users.noreply.github.com>
2023-11-23 21:08:06 +0530

    Update ShoppingChange.cpp

--- ShoppingChange.cpp
+++ ShoppingChange.cpp
@@ -2,7 +2,7 @@
 //      1 ≤ X ≤ 100). Unfortunately, Vineet only has a single 100 rupees note.
 // Since Vineet is weak at maths, can you help Vineet in calculating what money he should get back after paying 100 rupees for those items?
  
-#include <iostream>
+#include <iostream> 
 using namespace std;
 
 int main()
